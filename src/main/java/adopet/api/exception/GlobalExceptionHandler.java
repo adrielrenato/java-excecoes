af801b2bd0ca9b4aca2b2959b51023d83edebccc
Adriel Renato <adriel.renato26@gmail.com>
2025-03-21 17:18:41 -0300

    Criando um retorno global de exceções genericas.

--- src/main/java/adopet/api/exception/GlobalExceptionHandler.java
+++ src/main/java/adopet/api/exception/GlobalExceptionHandler.java
@@ -1,13 +1,33 @@
 package adopet.api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AdocaoException.class)
-    public ResponseEntity<String> adocaoException(AdocaoException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<ResponseError> adocaoException(AdocaoException ex) {
+        ResponseError response = new ResponseError(
+            ex.getMessage(),
+            HttpStatus.BAD_REQUEST,
+            LocalDateTime.now()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseError> trataException(Exception ex) {
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.badRequest().body(response);
     }
 }
